e97a8186dc1553db90841df83d3b371aa123d97d
Matt Aguiar <matt@revelstoke.io>
2022-11-02 14:00:19 -0400

    Attempt to get team by name

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import core from '@actions/core'
+import github from '@actions/github'
 
 try {
   const octokit = github.getOctokit(core.getInput('github-token'))
-  //const payload = github.context.payload
+  const payload = github.context.payload
 
-  //const reviewers = core.getInput('reviewers').split(',')
   const org = core.getInput('org')
-  const team_reviewers = core.getInput('team-reviewers').split(',')
-  
-  const team_members = []
-  team_reviewers.forEach(async team => {
-     const members = await octokit.rest.teams.listMembersInOrg({
-      org,
-      team_slug: team
-    })
-    team_members.push(members.map(member => member.login))
-  })
-  console.log(`Team members: ${team_members}`)
+  const usersArray = core.getInput('reviewers').split(',')
+  const teamsArray = core.getInput('team-reviewers').split(',')
 
-  // const result = await octokit.rest.pulls.requestReviewers({
-  //   owner: payload.repository.owner.login,
-  //   repo: payload.repository.name,
-  //   pull_number: payload.pull_request.number,
-  //   reviewers,
-  //   team_reviewers
-  // })
+  const results = await Promise.all(teamsArray.map(async team => {
+    const slug = team.split('/')[1]
+    // const members = await octokit.rest.teams.listMembersInOrg({
+    //   org: 'RevelStokeSec',
+    //   team_slug: slug
+    // })
+
+    const members = octokit.rest.teams.getByName({
+      org,
+      team_slug,
+    });
+
+    console.log("Members: ", members)
+    return members.map(member => member.login)
+  }))
+
+  results.push(usersArray)
+  const reviewers = Array.from(new Set(results.flat()))
+
+  const result = await octokit.rest.pulls.requestReviewers({
+    owner: payload.repository.owner.login,
+    repo: payload.repository.name,
+    pull_number: payload.pull_request.number,
+    reviewers
+  })
 } catch (error) {
+  console.log(error)
   core.setFailed(error)
 }
